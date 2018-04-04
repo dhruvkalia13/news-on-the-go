2cf456320562739384d3703bd545424e8fb04c08
Dhruv Kalia <dhruvkalia13@gmail.com>
2018-04-04 18:46:52 +0530

    *Integrated Default android TTS (text-to-speech)

--- app/src/main/java/com/news/dhruvkalia/news_on_the_go/ArticleDetailActivity.java
+++ app/src/main/java/com/news/dhruvkalia/news_on_the_go/ArticleDetailActivity.java
@@ -2,6 +2,7 @@ package com.news.dhruvkalia.news_on_the_go;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +15,14 @@ import android.widget.TextView;
 import com.news.dhruvkalia.news_on_the_go.Model.Article;
 import com.squareup.picasso.Picasso;
 
-public class ArticleDetailActivity extends AppCompatActivity {
+
+import java.util.Locale;
+
+public class ArticleDetailActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private TextToSpeech textToSpeech;
+    private Article article;
+    private FloatingActionButton headsetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +34,9 @@ public class ArticleDetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
 
+        textToSpeech = new TextToSpeech(this, this);
         Intent intent = getIntent();
-        Article article = intent.getParcelableExtra("article");
+        article = intent.getParcelableExtra("article");
 
         TextView articleDetailTitle = findViewById(R.id.article_detail_title);
         TextView articleDetailAuthor = findViewById(R.id.article_detail_author);
@@ -60,13 +69,50 @@ public class ArticleDetailActivity extends AppCompatActivity {
             Log.e("ArticleAdapter", "IllegalArgumentException is " + ex);
         }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+         headsetButton = (FloatingActionButton) findViewById(R.id.fab);
+        headsetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+                speakOut();
             }
         });
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = textToSpeech.setLanguage(Locale.ENGLISH);
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                headsetButton.setEnabled(true);
+//                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    private void speakOut() {
+
+        String text = article.getDescription();
+
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
